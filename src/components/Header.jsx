756093280efe47dbd6991fd9b2f794efc834e2c9
Luqman Hassan123 <119392736+luqman-hassan123@users.noreply.github.com>
2024-11-25 18:41:29 +0500

    Added header and footer components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Header = ({title}) => {
+const Header = ({ title, subtitle, logo }) => {
   return (
-    <header> 
-    <h1>{title}</h1>
-      
+    <header className="text-center bg-light py-3">
+      <img src={logo} alt="Logo" className="mb-2" style={{ height: '60px' }} />
+      <h1 className="m-0">{title}</h1>
+      <p className="m-0">{subtitle}</p>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
