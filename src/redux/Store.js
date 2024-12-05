d6610ad778747796486af872846b21483fb1c3f0
Luqman Hassan123 <119392736+luqman-hassan123@users.noreply.github.com>
2024-12-05 16:28:52 +0500

    .

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import weatherReducer from './slices/WeatherSlice';
+import weatherReducer from './slices/weatherSlice';
 import currencyReducer from './slices/currencySlice';
 
 
